83fb609cf04dd932c5bcb3abb6277893f5c53011
israil64 <md3096720@gmail.com>
2023-10-08 21:41:31 +0530

    Initial commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var w = (c.width = window.innerWidth),
   hw = w / 2, // half-width
   hh = h / 2,
   opts = {
-    strings: ["HAPPY", "BIRTHDAY!", "Israil"],
+    strings: ["HAPPY", "BIRTHDAY!", "Sadaf"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
